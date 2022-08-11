28e7f3fb6676880dc84429e76ab228b7034c04df
wi1dcode <magosdk@icloud.com>
2022-08-11 11:06:10 +0200

    get data, post data, middleware

--- middlewares/verifyHero.js
+++ middlewares/verifyHero.js
@@ -1,9 +1,8 @@
 const superHeroes = require('../superHeroes')
 
 const verifyHero = (req, res, next) => {
-  const { slug, power} = req.params
+  const { slug } = req.params
   const hero = superHeroes.find(superHeroes => superHeroes.slug === slug)
-//   const powers = superHeroes.find(superHeroes => superHeroes.power === power)
   const heroIndex = superHeroes.findIndex(superHeroes => superHeroes.slug === slug)
 
   if (!hero) {
@@ -11,13 +10,13 @@ const verifyHero = (req, res, next) => {
     res.status(404).json('Hero not found')
   } else {
     req.hero = hero
-    // req.powers = powers
     req.heroIndex = heroIndex
     next()
   }
 
 
 }
+
 
 module.exports = {
   verifyHero: verifyHero
--- routes/heroes.js
+++ routes/heroes.js
@@ -17,8 +17,40 @@ app.get('/:slug/powers', verifyHero, (req, res) => {
     res.json(req.hero.power)
 })
 
+app.post('/', (req, res) => {
+  const { slug, name, power, color, isAlive, age, image } = req.body
+  const hero = {
+    slug: slug,
+    name: name,
+    power: power,
+    color: color,
+    isAlive: isAlive,
+    age: age,
+    image: image
+  }
+    
+  
+  
+  const existingHero = superHeroes.find((hero) => {
+    return hero.slug === slug
+  })
+  
+  
+  if (!existingHero) {
+    superHeroes.push(hero)
+  } else {
+    res.status(409).json("Hero already exists")
+  }
 
 
+    
+  res.json(hero)
+
+})
+
+app.put('/:slug/powers', verifyHero, (req, res) => {
+  res.json(req.hero.power)
+})
 
 
 
